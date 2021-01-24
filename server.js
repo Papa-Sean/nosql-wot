857f2a550d661ba5227082ee096dca525695e85b
Papa-Sean <smsugrue@gmail.com>
2021-01-23 22:37:20 -0500

    Initial commit and all my mistakes

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const chalk = require('chalk');
+
+var success = chalk.greenBright
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(morgan("tiny"))
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log(success("Listening on port " +PORT+ " Visit http://localhost:" +PORT));
+});

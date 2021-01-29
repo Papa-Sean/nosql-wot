dbe93c0ad6117dcf2bb9e94d8fd10557d997a4f0
Papa-Sean <smsugrue@gmail.com>
2021-01-29 16:37:14 -0500

    Third time... charm

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 app.use(morgan("tiny"))
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  process.env.MONGODB_URI || 'mongodb://localhost/workouts',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,

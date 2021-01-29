d79a6f938a941dde2325b3eaca0292e5ae86d193
Papa-Sean <smsugrue@gmail.com>
2021-01-29 16:19:03 -0500

    Connect

--- server.js
+++ server.js
@@ -16,13 +16,15 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(morgan("tiny"))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
